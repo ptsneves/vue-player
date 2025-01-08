603fb1758ec917d7dc8aa6970bfb185a24e841c5
Paulo Neves <paulo@myneves.com>
2025-01-08 19:25:47 +0100

    Export range
    Needed if using custom controls but want to use
    the already done range component from vue-player.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import player from './src/components/player.vue'
 import pButton from './src/components/button.vue'
+import range from './src/components/range.vue'
 
 export default player
-export { pButton }
+export { pButton, range }
