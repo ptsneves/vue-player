7eb8b8d473e69ac69e16ed15eee605547987f862
Paulo Neves <paulo@myneves.com>
2025-01-09 20:15:14 +0100

    fixup! Emit current-time and duration.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import player from './src/components/player.vue'
 import pButton from './src/components/button.vue'
-import range from './src/components/range.vue'
+import vuePlayerRange from './src/components/range.vue'
 
 export default player
-export { pButton, range }
+export { pButton, vuePlayerRange }
