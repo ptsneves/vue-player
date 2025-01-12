0325dc3adb3f20a233fa8891b9da57327ea459a8
Paulo Neves <paulo@myneves.com>
2025-01-12 22:50:00 +0100

    Export the controls component
    Allows for easy usage in the controls slot

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import player from './src/components/player.vue'
 import pButton from './src/components/button.vue'
+import vuePlayerControls from './src/components/controls.vue'
 import vuePlayerRange from './src/components/range.vue'
 
 export default player
-export { pButton, vuePlayerRange }
+export { pButton, vuePlayerRange, vuePlayerControls }
